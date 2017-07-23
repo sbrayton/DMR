0f35c1e3c3903f13c66d7f8ec5a9333b9dbacb95
Shameka Brayton <shameka.brayton@sphereco.com>
2017-07-23 09:43:05 -0400

    d

---
+++ Assignment02/sb6000_assignment02.R
@@ -0,0 +1,7 @@
+setwd("~/DropBox/NYU/02_Module/08_DMR/Assignment02")
+
+
+library("rmarkdown", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
+render("sb6000_assignment02.Rmd")
+
+
