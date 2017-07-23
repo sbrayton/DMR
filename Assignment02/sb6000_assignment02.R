dd057e7589482acc82397b22a0bcec940ef1ec31
Shameka Brayton <shameka.brayton@sphereco.com>
2017-07-23 12:58:26 -0400

    chose dataset, added the data to the markdown

--- Assignment02/sb6000_assignment02.R
+++ Assignment02/sb6000_assignment02.R
@@ -4,4 +4,7 @@ setwd("~/DropBox/NYU/02_Module/08_DMR/Assignment02")
 library("rmarkdown", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
 render("sb6000_assignment02.Rmd")
 
+install.packages('RMySQL')
+
+
 
