15afdf984a48b1e08a8f0dbe4eb8bff27baf0e0c
Shameka Brayton <shameka.brayton@sphereco.com>
2017-07-24 19:46:55 -0400

    added logo, working on having the columns cleansed and added progromatically

--- Assignment02/sb6000_assignment02.R
+++ Assignment02/sb6000_assignment02.R
@@ -6,5 +6,58 @@ render("sb6000_assignment02.Rmd")
 
 install.packages('RMySQL')
 
+library("stringi")
 
+d <- readLines(url("https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/heart-disease.names"))
+
+
+d <- d[127:235]
+
+head(d,2)
+tail(d,2)
+
+stri_trim(d)
+
+d[grep("26",d)]
+
+i<-10
+d[i]
+d[grep(paste0("^",i),d)[1]]
+
+sapply(1:76,function(i) d[grep(paste0("^",i),d) [i]] ) 
+
+tgtLines <- sapply(1:76,function(i) print(d[i:5])) 
+
+
+head(tgtLines,2)
+
+t<- readLines(url("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names"))
+
+t<- t[106:116]
+
+x<-t[grep("[ ]",t[1])]
+
+x<-strsplit(t, " ")[[2]]
+x[5]
+t<-stri_trim(t)
+
+x<-strsplit(t[1], " ")
+x[[1:2]]
+
+
+head(mydat[!complete.cases(mydat),],3)
+
+library(readr)
+
+u<- parse_integer(mydat$bare_nuclei,na="?")
+
+mydat$bare_nuclei<-u
+
+head(mydat[!complete.cases(mydat),],3)
+
+noNA.mydat <- centralImputation(mydat) 
+
+View(noNA.mydat)
+
+nrow(mydat[!complete.cases(mydat),])
 
